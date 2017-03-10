dc499d3e568d3aaf9427e1ff076e664e57a75412
Tiphaine Perra <perra_t@epitech.eu>
2017-03-10 19:19:17 +0100

    Remove Src directory

--- src/InputBundle/InputBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace InputBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class InputBundle extends Bundle
-{
-}
--- src/LiveBundle/LiveBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace LiveBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class LiveBundle extends Bundle
-{
-}
