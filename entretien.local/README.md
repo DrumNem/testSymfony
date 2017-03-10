b7ad2f3f47d8fbf3986ecc496888a9b4707db923
Tiphaine Perra <perra_t@epitech.eu>
2017-03-10 19:15:00 +0100

    [Symfony] Second commit stockage DB sans display

---
+++ entretien.local/README.md
@@ -0,0 +1,4 @@
+entretien.local
+===============
+
+A Symfony project created on March 8, 2017, 7:50 pm.
---
+++ entretien.local/src/InputBundle/InputBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace InputBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class InputBundle extends Bundle
+{
+}
